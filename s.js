f9d2e09168c659671370cb885e0039da6802c1d8
Omertis <36013948+Omertis@users.noreply.github.com>
2018-05-14 09:55:47 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,6 +1,10 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ fetchAllMembers: true });
+
 const Music = require('discord.js-musicbo t-addon');
 const music = new Music(client, {
     prefix: ".", 
     youtubeKey: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
   
   });
+client.login(process.env.BOT_TOKEN);
